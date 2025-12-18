54fc6beba2ac5ca5e5f75ef9d00f4c585d50127e
Utku Demir <utku@rilla.network>
2025-12-18 15:12:48 +1300

    Remove unused transaction support

--- crates/composable-indexes/src/core/mod.rs
+++ crates/composable-indexes/src/core/mod.rs
@@ -2,7 +2,6 @@ mod collection;
 mod index;
 mod query_result;
 mod shallow_clone;
-mod transaction;
 
 pub mod store;
 
@@ -10,7 +9,6 @@ pub use collection::*;
 pub use index::*;
 pub use query_result::*;
 pub use shallow_clone::*;
-pub use transaction::*;
 
 #[cfg(feature = "std")]
 pub(crate) type DefaultHasher = std::hash::RandomState;
--- crates/composable-indexes/src/core/transaction.rs
+++
@@ -1,5 +0,0 @@
-use crate::Collection;
-
-pub struct Transaction<In, Ix, S> {
-    pub collection: Collection<In, Ix, S>,
-}
