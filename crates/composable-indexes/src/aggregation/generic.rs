36d73fbe84f0632329f7415cb980c81d32783373
Utku Demir <me@utdemir.com>
2025-12-30 20:55:25 +1300

    Fix MonoidalAggregate

--- crates/composable-indexes/src/aggregation/generic.rs
+++ crates/composable-indexes/src/aggregation/generic.rs
@@ -3,7 +3,27 @@ use crate::{
     core::{Index, Insert, Remove, Seal, Update},
 };
 
-/// A convenience struct that implements an aggregation given state operations.
+/// Implements an `Index` given state operations.
+///
+/// Example:
+///
+/// ```rust
+/// use composable_indexes::{Collection, aggregation};
+/// let sum_aggregate = aggregation::GenericAggregate::new(
+///     // initial state
+///     0,
+///     // query function
+///     |state: &i64| *state,
+///     // insert function
+///     |state: &mut i64, value: &i64| *state += *value,
+///     // remove function
+///     |state: &mut i64, value: &i64| *state -= *value,
+/// );
+/// let mut collection = Collection::new(sum_aggregate);
+/// collection.insert(2);
+/// collection.insert(3);
+/// assert_eq!(collection.query(|ix| ix.get()), 5);
+/// ```
 #[derive(Clone)]
 pub struct GenericAggregate<In, Query, State> {
     current_state: State,
@@ -54,7 +74,31 @@ impl<In, Query: Clone, State> GenericAggregate<In, Query, State> {
 
 impl<In: Clone, Query: Clone, State: Clone> ShallowClone for GenericAggregate<In, Query, State> {}
 
-/// A convenience struct that implements an aggregation given a monoidal structure.
+/// Given an algebraic group, implement an [`Index`].
+///
+/// Example:
+///
+/// ```rust
+/// use composable_indexes::{Collection, aggregation};
+///
+/// let number_of_a = aggregation::MonoidalAggregate::new(
+//      // empty value
+///     0,
+///     // input function
+///     |x: &String| x.chars().filter(|&c| c == 'a').count() as i64,
+///     // combine function
+///     |a: &i64, b: &i64| a + b,
+///     // invert function
+///     |a: &i64| -a,
+///     // output function
+///     |a: &i64| *a,
+/// );
+///
+/// let mut collection = Collection::new(number_of_a);
+/// collection.insert("apple".to_string());
+/// collection.insert("banana".to_string());
+/// assert_eq!(collection.query(|ix| ix.get()), 4);
+/// ```
 pub struct MonoidalAggregate<T, S, O> {
     state: S,
     input: fn(&T) -> S,
@@ -104,6 +148,20 @@ impl<T, S, O> Index<T> for MonoidalAggregate<T, S, O> {
         let old_ = (self.input)(op.existing);
         let new_ = (self.input)(op.new);
         let diff = (self.combine)(&(self.invert)(&old_), &new_);
-        (self.combine)(&self.state, &diff);
+        self.state = (self.combine)(&self.state, &diff);
     }
 }
+
+impl<T, S: Clone, O> Clone for MonoidalAggregate<T, S, O> {
+    fn clone(&self) -> Self {
+        MonoidalAggregate {
+            state: self.state.clone(),
+            input: self.input,
+            combine: self.combine,
+            invert: self.invert,
+            output: self.output,
+        }
+    }
+}
+
+impl<T, S: Clone, O> ShallowClone for MonoidalAggregate<T, S, O> {}
